69df62795aea137a0c822ee5a60afe0313c0569d
angavalos <angavalos@ucdavis.edu>
2023-07-27 14:50:14 -0700

    python script to remove outgroup from list of all Lactococcus. snakemake and shell scripts to run roary and sourmash on the resulting 365 Lactococcus

--- python/final-lacto-name-test.py
+++ python/final-lacto-name-test.py
@@ -2,7 +2,16 @@ import os
 import glob
 
 filename = "/group/weimergrp/aavalos7/projects/all-lactococcus/final-lacto-gt90completion-lt5contamination-gt50percentlacto.txt"
+outgroup = "/group/weimergrp/aavalos7/projects/all-lactococcus/lacto-outgroup-labels.txt"
 
 with open(filename) as file:
     lines = [line.rstrip() for line in file]
-print(lines)
+
+with open(outgroup) as file:
+    lines_out = [line.rstrip() for line in file]
+
+print(len(lines))
+print(len(lines_out))
+
+final_lines = [i for i in lines if i not in lines_out]
+print(len(final_lines))
